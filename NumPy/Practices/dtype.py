6454c91c1c8687a5e2e88aa7c5eab1c39bc57872
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2025-07-06 00:55:09 +0530

    added

---
+++ NumPy/Practices/dtype.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Complex data type array
+numpy_array =np.array([(0,1, 1), (2, 3, 5), (8, 13, 21)], dtype = complex)
+print(numpy_array)
+
+# Float data typr array
+numpy_array =np.array([(0,1, 1), (2, 3, 5), (8, 13, 21)], dtype = float)
+print(numpy_array)
+
+
+# String data type array
+numpy_array =np.array([(0,1, 1), (2, 3, 5), (8, 13, 21)], dtype = str)
+print(numpy_array)
