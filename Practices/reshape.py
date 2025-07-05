621a9a8e4d0349188d7234c9739231d66b6483d4
Shalini Kumari <159770762+angel-shalu@users.noreply.github.com>
2025-07-06 01:35:26 +0530

    Add files via upload

--- Practices/reshape.py
+++ Practices/reshape.py
@@ -10,3 +10,20 @@ print()
 numpy_array= np.array([0.577,1.618, 2.718, 3.14, 6, 28, 37, 1729]).reshape(4,2)
 print(numpy_array)
 print(numpy_array.dtype)
+
+
+
+# Reshape method()
+# Step 1: Create an array of numbers from 1 to 36 (37 excluded)
+# Step 2: Reshape it into a 2D array with 6 rows and 6 columns (6×6 = 36 elements)
+# Step 3: Print the reshaped array
+
+numpy_arange = np.arange(1, 37)   # Numbers from 1 to 36
+numpy_arange = np.reshape(numpy_arange, (6, 6))
+print(numpy_arange)
+
+# Same code with anothe way
+numpy_arange= np.arange(1, 37).reshape(6,6)
+print(numpy_arange)
+print(numpy_arange.size)  # Print the size of the element
+
